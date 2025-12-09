9ec737d58036ea80b3724b3e9d84166ebfadb70b
kuriki sachiyo <satuyo8@gmail.com>
2025-12-09 15:52:01 +0900

    fix:Deviseの:remenberableを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
 end
