49a5d593bf7fc25af429f9a78afb080cc941d444
kuriki sachiyo <satuyo8@gmail.com>
2025-12-14 08:37:36 +0900

    add:職員編集画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get "staffs/index"
-  get "staffs/new"
   get "dashboards/index"
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   root to: 'pages#home'
   get 'dashboard', to: 'dashboards#index'
-  resources :staffs, only: [:index, :new, :create]
+  resources :staffs, only: [:index, :new, :create, :edit, :update, :destroy]
 end
