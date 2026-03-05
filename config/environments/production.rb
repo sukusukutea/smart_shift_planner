4e1a116a9352dfcc5e64b8fd4670df534ef9cb60
kuriki sachiyo <satuyo8@gmail.com>
2026-03-05 20:17:18 +0900

    許可するホストを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts << "mimosa-shift-planner.onrender.com"
+  config.hosts << "smart-shift-planner.onrender.com"
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
